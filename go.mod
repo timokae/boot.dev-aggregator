f83ac6f4e98a553ab4165ccf017c6915bda3fad5
timokae <timo2503k@gmail.com>
2024-06-09 19:54:38 +0200

    boilerplate

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/timokae/boot.dev-aggregator
 
 go 1.22.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	port := os.Getenv("PORT")
+	if err != nil {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    "localhost:" + port,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /v1/healthz", func(w http.ResponseWriter, r *http.Request) {
+		respondWithJSON(w, http.StatusOK, struct {
+			Status string
+		}{
+			Status: "ok",
+		})
+	})
+
+	mux.HandleFunc("GET /v1/err", func(w http.ResponseWriter, r *http.Request) {
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	})
+
+	log.Printf("Serving on port: %s\n", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
